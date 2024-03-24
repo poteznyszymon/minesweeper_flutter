098f5d497a7ab6b7332e6e322f80f494fa73882b
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2024-03-24 22:00:29 +0100

    Update bug with time reset

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -95,6 +95,7 @@ class _HomePageState extends State<HomePage> {
         initializeFlagedStates();
         time = 0;
         flagsTotal = 10;
+        timer.cancel();
         timeStart();
       });
     }
